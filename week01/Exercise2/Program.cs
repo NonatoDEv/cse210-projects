2f02f528fef741948fd26485a6b9ec2105f79dfc
NonatoDEv <anonatoavalos@byupathway.edu>
2026-03-09 22:01:28 -0500

    week01 exercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,61 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("what is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        int lastDigit = grade % 10;
+        string sign = "";
+        if (lastDigit >=7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        if (letterGrade == "A" && (sign == "+" || grade == 100))
+        {
+            sign = "";
+        }
+        else if (letterGrade == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your letter grade is: {letterGrade} {sign}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard and don't give up!");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int attempts = 0;
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your magic number?");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+            if (attempts <= 7)
+                if(magicNumber > guess)
+                {
+                    Console.WriteLine("higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You guessed the magic number! and it only took you {attempts} attempts!");
+                }
+            else
+            {
+                Console.WriteLine("Sorry, you have used all 7 attempts. Better luck next time!");
+                Console.WriteLine($"The magic number was: {magicNumber}");
+                break;
+            }
+        }
     }
 }
