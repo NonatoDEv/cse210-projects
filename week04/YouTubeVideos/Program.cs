ec75ecdacff170f480c1ee76998699d742dc640d
NonatoDEv <anonatoavalos@byupathway.edu>
2026-03-29 19:00:54 -0500

    Add Video and Comment classes with methods for managing video comments

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -7,3 +7,52 @@ class Program
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
     }
 }
+class Video
+{
+    //only put the attributes
+    private string _title;
+    private string _author;
+    private int _videoLength;
+    //only puts the constructor
+    private List<Comment> _videoComments = new List<Comment>();
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _videoLength = length;
+    }
+    //only put the methods
+    public void AddComment(Comment comment){
+        _videoComments.Add(comment);
+    }
+    public int GetCommentCount(){
+        return _videoComments.Count;
+    }
+    public void DisplayInfo(){
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_videoLength}");
+        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
+        foreach(Comment comment in _videoComments)
+        {
+            Console.WriteLine($"{comment.GetDisplayText()}");
+        }
+    }
+}
+class Comment
+{
+    //once again put only the attributes
+    private string _commenterName;
+    private string _commentText;
+    //put the constructor
+    public Comment(string name, string text)
+    {
+        _commenterName = name;
+        _commentText = text;
+    }
+    //put the methods
+    public string GetDisplayText()
+    {
+        return $"{_commenterName}: {_commentText}";
+    }
+}
