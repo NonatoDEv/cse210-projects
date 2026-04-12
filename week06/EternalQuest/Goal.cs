a885546bb8b21ad64c335dee7f056f340a0fd3eb
NonatoDEv <anonatoavalos@byupathway.edu>
2026-04-12 18:10:14 -0500

    Refactor Goal class to use private fields and add SimpleGoal implementation for goal management

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -1,19 +1,22 @@
 public abstract class Goal
 {
-    protected string _shortName;
-    protected string _description;
-    protected int _points;
+    private string _shortName;
+    private string _description;
+    private int _points;
     public Goal(string name, string description, int points)
     {
         _shortName = name;
         _description = description;
         _points = points;
     }
+    public string ShortName => _shortName;
+    public string Description => _description;
+    public int Points => _points;
     public abstract void RecordEvent();
     public abstract bool IsComplete();
+    public abstract string GetStringRepresentation();
     public virtual string GetDetailsString()
     {
         return $"[{ (IsComplete() ? "X" : " ") }] {_shortName} ({_description})";
     }
-    public abstract string GetStringRepresentation();
 }
