29d99764eeb208e58c12595eaa235f1fed09c63a
NonatoDEv <anonatoavalos@byupathway.edu>
2026-04-12 19:38:09 -0500

    Implement GoalManager class with methods for goal management and user interaction

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -1,1 +1,90 @@
-    
+using System;
+using System.Collections.Generic;
+using System.IO;
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+    public GoalManager()
+    {
+        _goals = new List<Goal>();
+        _score = 0;
+    }
+    public void Start()
+    {
+        string input  = "";
+        while (input != "6")
+        {
+            Console.WriteLine();
+            DisplayPlayerInfo();
+            Console.WriteLine("\nMenu Options:");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            
+            input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    CreateGoal();
+                    break;
+                case "2":
+                    ListGoalDetails();
+                    break;
+                case "3":
+                    SaveGoals();
+                    break;
+                case "4":
+                    LoadGoals();
+                    break;
+                case "5":
+                    RecordEvent();
+                    break;
+                case "6":
+                    Console.WriteLine("¡Thanks for working today, see ya!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, please try again.");
+                    break;
+            }
+        } 
+    }
+    public void DisplayPlayerInfo()
+    {
+        Console.WriteLine($"You have {_score} points.");
+    }
+    public void ListGoalNames()
+    {
+        for (int i = 0; i < _goals.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {_goals[i].ShortName}");
+        }
+    }
+    public void ListGoalDetails()
+    {
+        Console.WriteLine("The goals are:");
+        foreach (Goal goal in _goals)
+        {
+            // Polymorphism in action: each goal type can have its own details string
+            Console.WriteLine(goal.GetDetailsString());
+        }
+    }
+    public void CreateGoal()
+    {
+    }
+
+    public void SaveGoals()
+    {
+    }
+    public void LoadGoals()
+    {
+    }
+    public void RecordEvent()
+    {
+    }
+}
